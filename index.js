658ca8e4b45ab63c84ca5b99c33f74a05cf87209
ysk-9 <yaswanthsaikiran9@gmail.com>
2022-05-10 15:36:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function loadCoupon(){
+    document.getElementById("coupon").style.display="block"
+
+}
+function closeCoupon(){
+    document.getElementById("coupon").style.display="none"
+    
+}
